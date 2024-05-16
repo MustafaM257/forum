c4fabcf788299e96c78597388c169ec49ad0a762
mustafa <mustafa.m257@proton.me>
2024-05-16 02:35:36 -0400

    Implement container reusable component

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -1,8 +1,19 @@
 <template>
     <AppLayout>
-Yo
+<Container>
+        <ul class="space-y-4">
+            <li v-for="post in posts">
+                {{post.title}}
+            </li>
+        </ul>
+</Container>
     </AppLayout>
 </template>
 <script setup lang="ts">
+import Container from "@/Components/Container.vue";
+
+defineProps([
+    'posts'
+]);
 import AppLayout from "@/Layouts/AppLayout.vue";
 </script>

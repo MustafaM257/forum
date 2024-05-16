a2deab5320b3f2a846ef6d6b9fccf797fe326db1
mustafa <mustafa.m257@proton.me>
2024-05-16 00:41:12 -0400

    Fill migrations and relations between Models

--- database/migrations/2024_05_16_043857_create_comments_table.php
+++ database/migrations/2024_05_16_043857_create_comments_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -12,14 +13,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignIdFor( User::class)->constrained()->restrictOnDelete();
+            $table->foreignIdFor(User::class)->constrained()->restrictOnDelete();
+            $table->foreignIdFor(Post::class)->constrained()->cascadeOnDelete();
 
-            $table->string('title');
             $table->longText('body');
-
+            
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 };

14c9b9bf5cef78a51b65c6d223076e60122834b9
mustafa <mustafa.m257@proton.me>
2024-05-16 23:39:43 -0400

    Add library for date formatting, style posts/show a bit

--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -1,8 +1,23 @@
 <template>
-    <AppLayout>
-        Hellooo
+    <AppLayout :title="post.title">
+        <h1>{{post.title}}</h1>
+        <article>
+            {{post.body}}
+        </article>
+        {{date}}
     </AppLayout>
 </template>
 <script setup lang="ts">
+import {computed} from "vue";
 import AppLayout from "@/Layouts/AppLayout.vue";
+import {formatDistance, parseISO} from "date-fns";
+const props = defineProps(['post'])
+
+const date = computed(() => {
+    return formatDistance(parseISO(props.post.created_at), new Date());
+})
+
+
+
+
 </script>

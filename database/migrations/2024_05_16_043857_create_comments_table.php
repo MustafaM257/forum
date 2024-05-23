82eecc53a45d346cc6ff6ad49dbec78ac31c9400
mustafa <mustafa.m257@proton.me>
2024-05-23 03:01:39 -0400

    Add html column to comments, boot statically

--- database/migrations/2024_05_16_043857_create_comments_table.php
+++ database/migrations/2024_05_16_043857_create_comments_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->foreignIdFor(Post::class)->constrained()->cascadeOnDelete();
 
             $table->longText('body');
-
+            $table->longText('html');
             $table->timestamps();
         });
     }
